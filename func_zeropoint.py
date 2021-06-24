4e9d30a27d5b208210099ce97f3f4fbef67ad10f
sndik16 <sndik16@gmail.com>
2021-06-24 09:18:39 -0700

    Update func_zeropoint.py
    created a function that takes an instrumental apparent magnitude and converts it to the calibrated apparent magnitude

--- func_zeropoint.py
+++ func_zeropoint.py
@@ -1,1 +1,9 @@
-
+def calibrated_apparent_magnitude(instrumental_magnitude, zeropoint):
+  """ 
+  This function takes a measured instrumental magnitude, \
+  and a zeropoint correction from standard stars in the fild \
+  and outputs the calibrated apparent magnitude.
+  """
+  calibrated_apparent_magnitude = zeropoint + instrumental_magnitude
+  
+  return calibrated_apparent_magnitude

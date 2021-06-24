b7cf00d574459edbc0f57761aa64848fb5868c10
sndik16 <sndik16@gmail.com>
2021-06-24 10:34:59 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 # importing functions
+import numpy as np
 from func_zeropoint import calibrated_apparent_magnitude
 from func import distance_to_parsec
 from func_kp import luminosity_calculator
@@ -9,7 +10,7 @@ photometric_band = input()
 instrumental_magnitude = input()
 zeropoint = input()
 distance = input()
-
+exponent_factor = input()
 
 #step1. calculate the calibrated apparent magnitude galaxy
 gal_calibrated_app_mag = calibrated_apparent_magnitude(instrumental_magnitude, zeropoint)
@@ -19,5 +20,11 @@ gal_abs_mag = distance_to_parsec(distance)
 
 #step3. calculate the luminosity of the galaxy
 gal_lum = luminosity_calculator(gal_abs_mag)
+
+#step4. calculate the mass of the galaxy
+gal_mass = mass_galaxy(gal_lum, exponent_factor)
+
+np.savetxt(galaxy_mass_out.txt, gal_mass)
+
 
 
